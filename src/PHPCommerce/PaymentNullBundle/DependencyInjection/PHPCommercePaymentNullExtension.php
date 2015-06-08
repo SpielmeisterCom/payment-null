f0586ea07f8967f1f3823c2b042f8a94ef6e5948
Julian Haupt <julian.haupt@abfallscout.de>
2015-06-08 10:39:54 +0200

    added configuration

---
+++ src/PHPCommerce/PaymentNullBundle/DependencyInjection/PHPCommercePaymentNullExtension.php
@@ -0,0 +1,21 @@
+<?php
+namespace PHPCommerce\PaymentBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+
+class PHPCommercePaymentNullExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('payment-null-gateway.yml');
+    }
+
+    public function getAlias()
+    {
+        return 'php_commerce_payment_null';
+    }
+}
---
+++ src/PHPCommerce/PaymentNullBundle/PHPCommercePaymentNullBundle.php
@@ -0,0 +1,9 @@
+<?php
+namespace PHPCommerce\PaymentBundle;
+use PHPCommerce\PaymentBundle\DependencyInjection\Compiler\PaymentGatewayCompilerPass;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class PHPCommercePaymentBundle extends Bundle
+{
+}
