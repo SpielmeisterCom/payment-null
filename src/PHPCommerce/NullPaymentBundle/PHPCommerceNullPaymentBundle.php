1a4affb2168fc771e95b6edce30239926c5e9945
Julian Haupt <julian.haupt@abfallscout.de>
2015-06-08 12:02:33 +0200

    fixed name of PHPCommerceNullPaymentBundle

--- src/PHPCommerce/NullPaymentBundle/PHPCommerceNullPaymentBundle.php
+++ src/PHPCommerce/NullPaymentBundle/PHPCommerceNullPaymentBundle.php
@@ -4,6 +4,6 @@ use PHPCommerce\PaymentBundle\DependencyInjection\Compiler\PaymentGatewayCompile
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class PHPCommercePaymentBundle extends Bundle
+class PHPCommerceNullPaymentBundle extends Bundle
 {
 }
